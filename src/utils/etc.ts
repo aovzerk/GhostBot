6ba8620037c8d99f2fd8982943e9786ab74a604d
aovzerk <pikseloooooo@gmail.com>
2023-11-19 21:16:01 +0400

    Update etc.ts

--- src/utils/etc.ts
+++ src/utils/etc.ts
@@ -1,9 +1,13 @@
 import { Message } from "discord.js";
 
-export function deleteMsgAfterTimeout(msg: Message, ms: number) {
+export function deleteMsgAfterTimeout(msg: Message, ms: number, verbose = false) {
 	return setTimeout(async () => {
 		try {
 			await msg.delete();
-		} catch (_) { }
+		} catch (error) {
+			if (verbose) {
+				console.log(error);
+			}
+		}
 	}, ms);
 }
